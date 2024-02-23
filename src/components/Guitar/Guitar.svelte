d29bdddc5c1da3e0ecffc732f7491f66108a690f
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2024-02-23 09:58:05 +0000

    Moved tuning presets to utils folder

--- src/components/Guitar/Guitar.svelte
+++ src/components/Guitar/Guitar.svelte
@@ -2,6 +2,7 @@
   import notes from '../../utils/notes';
   import formatOrdinal from '../../utils/formatOrdinal';
   import getConsecutiveNotes from '../../utils/getConsecutiveNotes';
+  import tuningPresetsPerNumberOfStrings from '../../utils/tuningPresets';
 
   export let scale: string[] = [];
 
@@ -11,109 +12,6 @@
   let numberOfStrings = 6;
   let selectedPreset: string;
   let stringTunings = ['E', 'A', 'D', 'G', 'B', 'E'];
-
-  const tuningPresetsPerNumberOfStrings = [
-    null,
-    null,
-    null,
-    null,
-    [
-      {
-        name: 'Standard E Tuning',
-        value: 'standard-e-tuning',
-        stringTunings: ['E', 'A', 'D', 'G']
-      },
-      {
-        name: 'Flat E Tuning',
-        value: 'flat-e-tuning',
-        stringTunings: ['D#', 'G#', 'C#', 'F#']
-      },
-      {
-        name: 'Standard D Tuning',
-        value: 'standard-d-tuning',
-        stringTunings: ['D', 'G', 'C', 'F']
-      },
-      {
-        name: 'Drop D Tuning',
-        value: 'drop-d-tuning',
-        stringTunings: ['D', 'A', 'D', 'G']
-      },
-      {
-        name: 'Drop Flat D Tuning',
-        value: 'drop-flat-e-tuning',
-        stringTunings: ['C#', 'G#', 'C#', 'F#']
-      },
-      {
-        name: 'Drop C Tuning',
-        value: 'drop-c-tuning',
-        stringTunings: ['C', 'G', 'C', 'F']
-      },
-    ],
-    [
-      {
-        name: 'Standard B Tuning',
-        value: 'standard-b-tuning',
-        stringTunings: ['B', 'E', 'A', 'D', 'G']
-      },
-      {
-        name: 'Flat B Tuning',
-        value: 'flat-b-tuning',
-        stringTunings: ['A#', 'D#', 'G#', 'C#', 'F#']
-      },
-      {
-        name: 'Standard A Tuning',
-        value: 'standard-a-tuning',
-        stringTunings: ['A', 'D', 'G', 'C', 'F']
-      },
-      {
-        name: 'Drop A Tuning',
-        value: 'drop-a-tuning',
-        stringTunings: ['A', 'E', 'A', 'D', 'G']
-      },
-      {
-        name: 'Drop Flat A Tuning',
-        value: 'flat-b-tuning',
-        stringTunings: ['G#', 'D#', 'G#', 'C#', 'F#']
-      },
-      {
-        name: 'Drop G Tuning',
-        value: 'standard-a-tuning',
-        stringTunings: ['G', 'D', 'G', 'C', 'F']
-      }
-    ],
-    [
-      {
-        name: 'Standard E Tuning',
-        value: 'standard-e-tuning',
-        stringTunings: ['E', 'A', 'D', 'G', 'B', 'E']
-      },
-      {
-        name: 'Flat E Tuning',
-        value: 'flat-e-tuning',
-        stringTunings: ['D#', 'G#', 'C#', 'F#', 'A#', 'D#']
-      },
-      {
-        name: 'Standard D Tuning',
-        value: 'standard-d-tuning',
-        stringTunings: ['D', 'G', 'C', 'F', 'A', 'D']
-      },
-      {
-        name: 'Drop D Tuning',
-        value: 'drop-d-tuning',
-        stringTunings: ['D', 'A', 'D', 'G', 'B', 'E']
-      },
-      {
-        name: 'Drop Flat D Tuning',
-        value: 'drop-flat-e-tuning',
-        stringTunings: ['C#', 'G#', 'C#', 'F#', 'A#', 'D#']
-      },
-      {
-        name: 'Drop C Tuning',
-        value: 'drop-c-tuning',
-        stringTunings: ['C', 'G', 'C', 'F', 'A', 'D']
-      }
-    ]
-  ];
 
   type SelectedNote = {
     value: string;
