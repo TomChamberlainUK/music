2562110b22c368accebc71333395f1110b4ecf72
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2024-02-24 10:32:18 +0000

    Added theme control to menu and persisted settings between sessions

--- src/components/Layout/Layout.svelte
+++ src/components/Layout/Layout.svelte
@@ -1,5 +1,52 @@
 <script lang="ts">
   let menuIsOpen: boolean = false;
+  let theme: Theme = getTheme();
+
+  type Theme = 'system' | 'light' | 'dark';
+
+  $: setTheme(theme);
+
+  function getTheme() {
+    const savedTheme = localStorage.getItem('theme');
+
+    switch (savedTheme) {
+      case 'light':
+      case 'dark':
+        return savedTheme;
+      default:
+        return 'system';
+    }
+  }
+
+  function setTheme(theme: string) {
+    switch (theme) {
+      case 'system': {
+        const isDarkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        
+        if (isDarkTheme) {
+          document.documentElement.setAttribute('data-theme', 'dark');
+        } else {
+          document.documentElement.setAttribute('data-theme', 'light');
+        }
+
+        localStorage.removeItem('theme');
+        break;
+      }
+      case 'light': {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        break;
+      }
+      case 'dark': {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        break;
+      }
+      default: {
+        throw new Error(`Unknown theme received: ${theme}`);
+      }
+    }
+  }
 
   function toggleMenu() {
     menuIsOpen = !menuIsOpen;
@@ -25,8 +72,8 @@
     </button>
     Music
     <button
-      on:click={toggleDarkMode}
       class="header__button"
+      on:click={toggleDarkMode}
     >
       Toggle dark mode
     </button>
@@ -34,7 +81,24 @@
   <menu
     class="menu"
     class:menu--open={menuIsOpen}
-  />
+  >
+    <label>
+      <span>
+        Theme
+      </span>
+      <select bind:value={theme}>
+        <option value="system">
+          System
+        </option>
+        <option value="light">
+          Light
+        </option>
+        <option value="dark">
+          Dark
+        </option>
+      </select>
+    </label>
+  </menu>
   <div class="body">
     <slot />
   </div>
