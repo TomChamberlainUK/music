e50fc8aaeff9de364d44a2d626549afcd62831df
Tom Chamberlain <44293967+TomChamberlainUK@users.noreply.github.com>
2024-02-25 15:28:10 +0000

    #56 Match theme to user system settings (#59)
    * Applied user system theme settings on startup
    
    * Added empty side panel menu to Layout component
    
    * Added theme control to menu and persisted settings between sessions
    
    * Added extra width and padding to side panel menu and fixed outline in light theme
    
    * Removed unused theme button and util functions

--- src/components/Layout/Layout.svelte
+++ src/components/Layout/Layout.svelte
@@ -1,27 +1,91 @@
 <script lang="ts">
-  function toggleDarkMode() {
-    const darkModeIsEnabled = document.documentElement.getAttribute('data-theme') === 'dark';
-    if (!darkModeIsEnabled) {
-      document.documentElement.setAttribute('data-theme', 'dark');
-    } else {
-      document.documentElement.setAttribute('data-theme', 'light');
+  let menuIsOpen: boolean = false;
+  let theme: Theme = getTheme();
+
+  type Theme = 'system' | 'light' | 'dark';
+
+  $: setTheme(theme);
+
+  function getTheme() {
+    const savedTheme = localStorage.getItem('theme');
+
+    switch (savedTheme) {
+      case 'light':
+      case 'dark':
+        return savedTheme;
+      default:
+        return 'system';
     }
+  }
+
+  function setTheme(theme: string) {
+    switch (theme) {
+      case 'system': {
+        const isDarkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        
+        if (isDarkTheme) {
+          document.documentElement.setAttribute('data-theme', 'dark');
+        } else {
+          document.documentElement.setAttribute('data-theme', 'light');
+        }
+
+        localStorage.removeItem('theme');
+        break;
+      }
+      case 'light': {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        break;
+      }
+      case 'dark': {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        break;
+      }
+      default: {
+        throw new Error(`Unknown theme received: ${theme}`);
+      }
+    }
+  }
+
+  function toggleMenu() {
+    menuIsOpen = !menuIsOpen;
   }
 </script>
 
 <div class="container">
   <header class="header">
-    <button class="header__button">
+    <button
+      class="header__button"
+      on:click={toggleMenu}
+    >
       Menu
     </button>
-    Music
-    <button
-      on:click={toggleDarkMode}
-      class="header__button"
-    >
-      Toggle dark mode
-    </button>
+    <span>
+      Music
+    </span>
   </header>
+  <menu
+    class="menu"
+    class:menu--open={menuIsOpen}
+  >
+    <label>
+      <span>
+        Theme
+      </span>
+      <select bind:value={theme}>
+        <option value="system">
+          System
+        </option>
+        <option value="light">
+          Light
+        </option>
+        <option value="dark">
+          Dark
+        </option>
+      </select>
+    </label>
+  </menu>
   <div class="body">
     <slot />
   </div>
