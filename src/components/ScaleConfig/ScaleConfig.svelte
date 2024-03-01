4f26cdd32c31f416739bbc12a5a628596685ce40
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2024-03-01 15:05:21 +0000

    Added chromatic scale

--- src/components/ScaleConfig/ScaleConfig.svelte
+++ src/components/ScaleConfig/ScaleConfig.svelte
@@ -14,6 +14,9 @@
   type ScaleModePatterns = Record<string, number[]>;
 
   const patterns: Record<string, ScaleModePatterns> = {
+    chromatic: {
+      chromatic: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+    },
     diatonic: {
       ionian: [0, 2, 4, 5, 7, 9, 11],
       dorian: [0, 2, 3, 5, 7, 9, 10],
