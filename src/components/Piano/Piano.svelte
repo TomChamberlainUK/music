a426a20a6357050b2f76990070ecb5840d9cf222
Tom Chamberlain <44293967+TomChamberlainUK@users.noreply.github.com>
2024-03-04 22:18:47 +0000

    #1 Add piano (#63)
    * Added Piano component
    
    * Added background to piano note highlights
    
    * Widened Piano key widths to fix squashed indicators
    
    * Added shadow to highlight Piano note indicators
    
    * Emphasized Piano highlighted note text
    
    * Added Piano component to addable instruments

---
+++ src/components/Piano/Piano.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+  import type { SelectedNote } from '@/types';
+
+  export let highlightedNotes: SelectedNote[] = [];
+
+  const whiteKeys = [
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'A',
+    'B'
+  ];
+
+  const blackKeys = [
+    'C#',
+    'D#',
+    'F#',
+    'G#',
+    'A#'
+  ];
+
+  $: isHighlighted = (note: string) => (
+    highlightedNotes.some(({ value }) => value === note)
+  );
+
+  $: getHighlightedNote = (note: string) => (
+    highlightedNotes.find(({ value }) => value === note)
+  );
+</script>
+
+<div class="container">
+  <div class="piano">
+    <div class="white-keys">
+      {#each whiteKeys as note}
+        <div class="white-keys__key">
+          <div
+            class="white-keys__indicator"
+            class:white-keys__indicator--highlighted={isHighlighted(note)}
+            style={isHighlighted(note) ? `background-color: ${getHighlightedNote(note)?.color};` : undefined}
+          >
+            {note}
+          </div>
+        </div>
+      {/each}
+    </div>
+    <div class="black-keys">
+      {#each blackKeys as note}
+        <div class="black-keys__key">
+          <div
+            class="black-keys__indicator"
+            class:black-keys__indicator--highlighted={isHighlighted(note)}
+            style={isHighlighted(note) ? `background-color: ${getHighlightedNote(note)?.color};` : undefined}
+          >
+            {note}
+          </div>
+        </div>
+      {/each}
+    </div>
+  </div>
+</div>
+
+<style lang="scss">
+  @import './Piano.scss';
+</style>
