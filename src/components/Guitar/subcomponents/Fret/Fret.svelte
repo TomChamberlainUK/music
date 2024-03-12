2a945b14bd121cf6245321a69401fc23aadfcc40
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2024-03-12 22:47:54 +0000

    Added interval name tooltip to guitar frets

--- src/components/Guitar/subcomponents/Fret/Fret.svelte
+++ src/components/Guitar/subcomponents/Fret/Fret.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+  import type { Action } from 'svelte/action';
   import type { SelectedNote } from '@/types/SelectedNote';
+  import Tooltip from '@/components/Tooltip';
 
   export let note = 'E';
   export let selectedNote: SelectedNote | null = null;
@@ -28,12 +30,59 @@
   $: getHighlightedNote = (note: string) => (
     highlightedNotes.find(({ value }) => value === note)
   );
+
+  $: getIntervalName = (note:string) => (
+    getHighlightedNote(note)?.name
+  );
+
+  const tooltip: Action<HTMLElement, { text?: string }> = (node, { text }) => {
+    const tooltipElement = new Tooltip({
+      props: { details: text },
+      target: document.body
+    });
+
+    handleResize();
+
+    node.addEventListener('mouseover', handleMouseOver);
+    node.addEventListener('mouseleave', handleMouseLeave);
+    window.addEventListener('resize', handleResize);
+
+    function handleResize() {
+      const { x, y, width } = node.getBoundingClientRect();
+      tooltipElement.$set({
+        x: x + (width / 2),
+        y
+      });
+    }
+
+    function handleMouseOver() {
+      handleResize();
+      tooltipElement.$set({ isVisible: true });
+    }
+
+    function handleMouseLeave() {
+      tooltipElement.$set({ isVisible: false });
+    }
+
+    return {
+      update({ text }) {
+        tooltipElement.$set({ details: text });
+      },
+      destroy() {
+        node.removeEventListener('mouseover', handleMouseOver);
+        node.removeEventListener('mouseleave', handleMouseLeave);
+        window.removeEventListener('resize', handleResize);
+        tooltipElement.$destroy();
+      }
+    };
+  };
 </script>
 
 <button
   class="fret"
   title={getHighlightedNote(note)?.name}
   on:click={() => selectNote(note)}
+  use:tooltip={{ text: getIntervalName(note) }}
 >
   <div
     class:fret__indicator={isHighlighted(note) || isSelected(note)}
