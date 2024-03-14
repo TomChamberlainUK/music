366cff573dc5fe953523eaacda68e29a9eda41b0
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2024-03-14 19:49:34 +0000

    Fixed tooltip position when document is scrolled

--- src/actions/tooltip.ts
+++ src/actions/tooltip.ts
@@ -7,7 +7,8 @@ const tooltip: Action<HTMLElement, { text?: string }> = (node, { text }) => {
 
   node.addEventListener('mouseenter', handleMouseEnter);
   node.addEventListener('mouseleave', handleMouseLeave);
-  window.addEventListener('resize', handleResize);
+  window.addEventListener('resize', updateTooltipPosition);
+  window.addEventListener('scroll', updateTooltipPosition);
 
   function createTooltipElement() {
     const { x, y } = getTooltipPosition();
@@ -23,18 +24,18 @@ const tooltip: Action<HTMLElement, { text?: string }> = (node, { text }) => {
     tooltipElement = null;
   }
 
-  function getTooltipPosition() {
-    const nodeBounds = node.getBoundingClientRect();
-    return {
-      x: nodeBounds.x + (nodeBounds.width / 2),
-      y: nodeBounds.y
-    };
-  }
-
-  function handleResize() {
+  function updateTooltipPosition() {
     if (!tooltipElement) return;
     const { x, y } = getTooltipPosition();
     tooltipElement.$set({ x, y });
+  }
+
+  function getTooltipPosition() {
+    const nodeBounds = node.getBoundingClientRect();
+    return {
+      x: nodeBounds.x + (nodeBounds.width / 2) + window.scrollX,
+      y: nodeBounds.y + window.scrollY
+    };
   }
 
   function handleMouseEnter() {
@@ -53,7 +54,8 @@ const tooltip: Action<HTMLElement, { text?: string }> = (node, { text }) => {
     destroy() {
       node.removeEventListener('mouseenter', handleMouseEnter);
       node.removeEventListener('mouseleave', handleMouseLeave);
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener('resize', updateTooltipPosition);
+      window.removeEventListener('scroll', updateTooltipPosition);
       destroyTooltipElement();
     }
   };
