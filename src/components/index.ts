44b9fd95163e288931480249191d1fae5750a535
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2024-04-11 20:41:39 +0100

    Removed unused chart components

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,4 @@ export { default as CircleOfFifths } from './CircleOfFifths';
 export { default as Guitar } from './Guitar';
 export { default as Layout } from './Layout';
 export { default as Piano } from './Piano';
-export { default as PieChart } from './PieChart';
 export { default as ScaleConfig } from './ScaleConfig';
