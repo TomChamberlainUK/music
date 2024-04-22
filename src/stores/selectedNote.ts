931221909e2c8bc8024c8cb84fc39431ef261af5
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2024-04-22 18:58:56 +0100

    Set scale config to prefill with current note settings if available

--- src/stores/selectedNote.ts
+++ src/stores/selectedNote.ts
@@ -1,18 +1,29 @@
 import { writable } from 'svelte/store';
+import { highlightedNotes as highlightedNotesStore } from '@/stores';
 import type { SelectedNote } from '@/types';
 
 function createSelectedNote() {
   const { set, subscribe, update } = writable<SelectedNote | null>(null);
 
+  let highlightedNotes: SelectedNote[];
+
+  highlightedNotesStore.subscribe((value) => {
+    highlightedNotes = value;
+  });
+
   return {
     subscribe,
     select: (value: string) => update((currentSelection) => {
+      const {
+        name = '',
+        color = '#76a0ff'
+      } = highlightedNotes.find(note => note.value === value) ?? {};
       return currentSelection?.value === value
         ? null
         : {
           value,
-          name: '',
-          color: '#76a0ff'
+          name,
+          color
         };
     }),
     set,
