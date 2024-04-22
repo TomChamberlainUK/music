804eda7a4bc9342e8cb54b1e859224fa2a8dab44
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2024-04-22 21:59:11 +0100

    Set up svelte component testing infrastructure

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vitest/config';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  plugins: [svelte()],
+  resolve: {
+    conditions: mode === 'test' ? ['browser'] : []
+  },
   test: {
+    environment: 'jsdom',
+    setupFiles: ['./tests/vitest-setup.ts'],
     coverage: {
       reportsDirectory: './tests/coverage'
     }
   }
-});
+}));
