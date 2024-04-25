1a7046e294ed84a0e8061e74825c94232659ff46
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2024-04-25 21:26:36 +0100

    Added Dropdown component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as CircleOfFifths } from './CircleOfFifths';
+export { default as Dropdown } from './Dropdown';
 export { default as Guitar } from './Guitar';
 export { default as Layout } from './Layout';
 export { default as Piano } from './Piano';
