602fa32195e2ecaec2e7c07b8da9757b3cd64a8e
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2024-05-29 21:19:40 +0100

    Added Layout component tests

---
+++ tests/components/Layout/Layout.test.ts
@@ -0,0 +1,38 @@
+import { render, screen } from '@testing-library/svelte';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+import Layout from '@/components/Layout';
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // Deprecated
+    removeListener: vi.fn(), // Deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
+
+describe('<Layout />', () => {
+  beforeEach(() => {
+    render(Layout);
+  });
+
+  it('Should render', () => {
+    const layout = screen.getByTestId('layout');
+    expect(layout).toBeInTheDocument();
+  });
+
+  it('Should render a header', () => {
+    const header = screen.getByRole('banner')
+    expect(header).toBeInTheDocument();
+  });
+
+  it('Should render a menu', () => {
+    const menu = screen.getByRole('list');
+    expect(menu).toBeInTheDocument();
+  });
+});
