1fd30f2969998a2a3dafa4ddb893682269c43119
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2024-07-09 14:53:49 +0100

    Set instruments to be accessible list with basic headings and styles

--- src/components/Instruments/Instruments.svelte
+++ src/components/Instruments/Instruments.svelte
@@ -3,21 +3,37 @@
   import { instruments } from '@/stores';
 </script>
 
-<div>
+<ul class="list">
   {#each $instruments as { id, type } (id)}
-    {#if type === 'guitar'}
-      <Guitar />
-    {:else if type === 'piano'}
-      <Piano />
-    {/if}
-    <button on:click={() => instruments.remove(id)}>
-      Remove
-    </button>
+    <li aria-labelledby="instrument-label-{id}">
+      <h3
+        class="heading"
+        id="instrument-label-{id}"
+      >
+        {type}
+        {' '}
+        <span class="heading__id">
+          (id: {id})
+        </span>
+      </h3>
+      {#if type === 'guitar'}
+        <Guitar />
+      {:else if type === 'piano'}
+        <Piano />
+      {/if}
+      <button on:click={() => instruments.remove(id)}>
+        Remove
+      </button>
+    </li>
   {/each}
-</div>
+</ul>
 <button on:click={() => instruments.add('guitar')}>
   Add Guitar
 </button>
 <button on:click={() => instruments.add('piano')}>
   Add Piano
 </button>
+
+<style lang="scss">
+  @import './Instruments.scss';
+</style>
