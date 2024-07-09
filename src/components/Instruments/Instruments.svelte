9fe904a791fccd919e7dd39fa49f4813b2f28533
Tom Chamberlain <44293967+TomChamberlainUK@users.noreply.github.com>
2024-07-09 20:01:32 +0100

    #75 Added local save for instruments setup (#93)
    * Abstracted Instruments to its own component
    
    * Abstracted instruments to store
    
    * Set instruments to be accessible list with basic headings and styles
    
    * Added Instruments unit tests
    
    * Updated instrument store to use localStorage
    
    * Added reset to default button for instruments
    
    * Refactored for simplicity

---
+++ src/components/Instruments/Instruments.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import { Guitar, Piano } from '@/components';
+  import { instruments } from '@/stores';
+</script>
+
+<ul class="list">
+  {#each $instruments as { id, type } (id)}
+    <li aria-labelledby="instrument-label-{id}">
+      <h3
+        class="heading"
+        id="instrument-label-{id}"
+      >
+        {type}
+        {' '}
+        <span class="heading__id">
+          (id: {id})
+        </span>
+      </h3>
+      {#if type === 'guitar'}
+        <Guitar />
+      {:else if type === 'piano'}
+        <Piano />
+      {/if}
+      <button on:click={() => instruments.remove(id)}>
+        Remove
+      </button>
+    </li>
+  {/each}
+</ul>
+<button on:click={() => instruments.add('guitar')}>
+  Add Guitar
+</button>
+<button on:click={() => instruments.add('piano')}>
+  Add Piano
+</button>
+<button on:click={() => instruments.reset()}>
+  Reset to Default
+</button>
+
+<style lang="scss">
+  @import './Instruments.scss';
+</style>
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,4 +1,5 @@
 export { default as highlightedNotes } from './highlightedNotes';
+export { default as instruments } from './instruments';
 export { default as mode } from './mode';
 export { default as root } from './root';
 export { default as selectedNote } from './selectedNote';
