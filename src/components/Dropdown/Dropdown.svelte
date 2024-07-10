8fa9fa20b31e1549a8fe5b20e50514af2401affd
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2024-07-10 19:15:49 +0100

    Added compatibility for options with a different name and value

--- src/components/Dropdown/Dropdown.svelte
+++ src/components/Dropdown/Dropdown.svelte
@@ -1,7 +1,24 @@
 <script lang="ts">
   export let label: string;
-  export let options: string[] = [];
-  export let value: string = options[0];
+  export let options: Option[] = [];
+  export let value: string = getOptionValue(options[0]);
+
+  type Option = string | {
+    name: string;
+    value: string;
+  };
+
+  function getOptionValue(option: Option) {
+    return typeof option === 'string'
+      ? option
+      : option.value;
+  }
+
+  function getOptionName(option: Option) {
+    return typeof option === 'string'
+      ? option
+      : option.name;
+  }
 </script>
 
 <label>
@@ -13,8 +30,8 @@
     on:change
   >
     {#each options as option}
-      <option value={option}>
-        {option}
+      <option value={getOptionValue(option)}>
+        {getOptionName(option)}
       </option>
     {/each}
   </select>
