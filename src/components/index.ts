fab7126e2601d8753cae135f3c5fd6db3dc73074
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2024-07-10 21:01:17 +0100

    Moved Dropdown component to FormInputs directory

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,5 @@
 export { default as CircleOfFifths } from './CircleOfFifths';
-export { default as Dropdown } from './Dropdown';
+export { Dropdown } from './FormInputs';
 export { default as Guitar } from './Guitar';
 export { default as Layout } from './Layout';
 export { default as Piano } from './Piano';
