7959f7bc781a23716fc41e650b24a76e947adf06
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2024-07-22 21:05:15 +0100

    Refactored typing for simplicity

--- src/utils/getRange.ts
+++ src/utils/getRange.ts
@@ -1,13 +1,21 @@
+type Options = {
+  format?: 'string' | 'number';
+};
+
 function getRange(min: number, max: number): number[];
-function getRange<T extends 'string' | 'number' = 'number'>(min: number, max: number, options?: { format?: T }): (T extends 'string' ? string : number)[];
+function getRange<T extends Options['format'] = 'number'>(
+  min: number,
+  max: number,
+  options: {
+    format?: T
+  }
+): (T extends 'string' ? string : number)[];
 function getRange(
   min: number,
   max: number,
   {
     format
-  }: {
-    format?: 'string' | 'number'
-  } = {}
+  }: Options = {}
 ) {
   const range = [
     ...Array(max + 1).keys()
