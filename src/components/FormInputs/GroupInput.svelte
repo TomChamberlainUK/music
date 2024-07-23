1cf4e66326e3754c8300c47ca3f677183c2fbf0c
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2024-07-23 17:51:37 +0100

    Removed fieldset and legend handling from GroupInput

--- src/components/FormInputs/GroupInput.svelte
+++ src/components/FormInputs/GroupInput.svelte
@@ -1,16 +1,11 @@
 <script lang="ts">
   import InputWrapper from './InputWrapper.svelte';
 
-  export let label: string;
   export let checkboxes: string[];
   export let group: string[];
 </script>
 
-<fieldset>
-  <legend>
-    {label}
-  </legend>
-  {#each checkboxes as checkbox}
+{#each checkboxes as checkbox}
   <InputWrapper
     label={checkbox}
   >
@@ -20,5 +15,4 @@
       bind:group
     />
   </InputWrapper>
-  {/each}
-</fieldset>
+{/each}
