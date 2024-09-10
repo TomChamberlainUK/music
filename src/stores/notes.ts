ed6d860f308cad9eb436e0290e051465d15691d4
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2024-09-10 21:37:37 +0100

    Set up dynamic scale using stores

---
+++ src/stores/notes.ts
@@ -0,0 +1,16 @@
+import { readable } from 'svelte/store';
+
+export default readable([
+  'A',
+  'A♯',
+  'B',
+  'C',
+  'C♯',
+  'D',
+  'D♯',
+  'E',
+  'F',
+  'F♯',
+  'G',
+  'G♯'
+]);
--- src/stores/scale.ts
+++ src/stores/scale.ts
@@ -1,11 +1,13 @@
-import { writable } from 'svelte/store';
+import { derived } from 'svelte/store';
+import notes from './notes';
+import scalePatterns from './scalePatterns';
+import root from './root';
+import { getNotesFromRoot } from '@/utils';
+import scaleName from './scaleName';
+import modeName from './modeName';
 
-export default writable<string[]>([
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'A',
-  'B'
-]);
+export default derived([root, notes, scalePatterns, scaleName, modeName], ([$root, $notes, $scalePatterns, $scaleName, $modeName]) => {
+  const notes = getNotesFromRoot($root, $notes);
+  const scalePattern = $scalePatterns[$scaleName][$modeName] ?? [];
+  return scalePattern.map(interval => notes[interval]);
+});
