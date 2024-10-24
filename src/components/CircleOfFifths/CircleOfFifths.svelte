04da37af6dfe7c24e4cb9293ad0f4a5e3ffa0f14
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2024-10-24 16:02:01 +0100

    Set CircleOfFifths component to use scale store

--- src/components/CircleOfFifths/CircleOfFifths.svelte
+++ src/components/CircleOfFifths/CircleOfFifths.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { getFifthsFromRoot } from '@/utils';
   import { getFifthsShapes, getModeOffset } from './utils';
-  import { mode, root } from '@/stores';
+  import { scale } from '@/stores';
 
   const majorFifths = [
     'C',
@@ -55,8 +55,8 @@
   const height = '15rem';
 
   $: fifthsFromC = getFifthsFromRoot('C');
-  $: rootIndex = fifthsFromC.findIndex(value => value === $root);
-  $: modeOffsetIndex = rootIndex + getModeOffset($mode);
+  $: rootIndex = fifthsFromC.findIndex(value => value === $scale.root);
+  $: modeOffsetIndex = rootIndex + getModeOffset($scale.modeName);
   $: fifthsShapes = getFifthsShapes({
     allFifths: [...majorFifths, ...minorFifths, ...diminishedFifths],
     highlightOffset: modeOffsetIndex,
